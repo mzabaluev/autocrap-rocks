7746f65dc98bf1ca505d02ee5c1e4c6a3101c9d1
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2011-11-21 23:10:03 +0200

    Forgot proper setup and teardown in tests

--- tests/crap-test.c
+++ tests/crap-test.c
@@ -1,8 +1,11 @@
+#include <kanye/kanye.h>
 #include <crap/crap.h>
 
 int main()
 {
+    kanye_storm_stage();
     crap();
+    kanye_drag_off_stage();
     return 0;
 }
 
--- tests/old-test.c
+++ tests/old-test.c
@@ -1,8 +1,11 @@
+#include <kanye/kanye.h>
 #include <old/old.h>
 
 int main()
 {
+    kanye_storm_stage();
     old();
+    kanye_drag_off_stage();
     return 0;
 }
 
